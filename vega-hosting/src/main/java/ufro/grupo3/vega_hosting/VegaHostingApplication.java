c025dfc06a18b1c56ebb337ddd737fb6390fd813
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-18 16:19:24 -0400

    Creacion del proyecto springboot

---
+++ vega-hosting/src/main/java/ufro/grupo3/vega_hosting/VegaHostingApplication.java
@@ -0,0 +1,13 @@
+package ufro.grupo3.vega_hosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegaHostingApplication.class, args);
+	}
+
+}
---
+++ vega-hosting/src/test/java/ufro/grupo3/vega_hosting/VegaHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.grupo3.vega_hosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegaHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
