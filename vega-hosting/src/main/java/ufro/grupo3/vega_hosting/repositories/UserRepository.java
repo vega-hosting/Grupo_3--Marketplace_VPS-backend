aeb7cff32284459e12f89eee8610d85929be8424
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-06-22 18:42:22 -0400

    Creacion Repositorio UserRepository

---
+++ vega-hosting/src/main/java/ufro/grupo3/vega_hosting/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package ufro.grupo3.vega_hosting.repositories;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
