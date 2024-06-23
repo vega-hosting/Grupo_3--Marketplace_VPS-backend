83dccc812ddbe8fd8549ca76293ea21b117b4ff4
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-23 16:08:09 -0400

    Update SubscriptionRepository.java
    Se modifico el parametro de de busqueda personalizada.

--- vega-hosting/src/main/java/ufro/grupo3/vega_hosting/repositories/SubscriptionRepository.java
+++ vega-hosting/src/main/java/ufro/grupo3/vega_hosting/repositories/SubscriptionRepository.java
@@ -2,16 +2,14 @@ package ufro.grupo3.vega_hosting.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ufro.grupo3.vega_hosting.models.Plan;
 import ufro.grupo3.vega_hosting.models.Subscription;
-import ufro.grupo3.vega_hosting.models.User;
 
 import java.util.List;
 
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
 
-    List<Subscription> findByPlan(Plan plan);
+    List<Subscription> findByPlanId(Integer planId);
 
-    List<Subscription> findByUser(User user);
+    List<Subscription> findByUserId(Integer userId);
 }
